8a311375301bf84e747a8993b0e2104bb960fac6
IAPigarev <iapigarev@gmail.com>
2022-11-30 22:19:13 +0300

    added task 7

--- task6/Program.cs
+++ task6/Program.cs
@@ -3,7 +3,6 @@
 // -3 -> нет
 // 7 -> нет
 Console.WriteLine("Введите число:");
-//string answer = Console.ReadLine();
 int number = Convert.ToInt32(Console.ReadLine());
 if (number%2 > 0||number%2 < 0) Console.WriteLine("Число не четное.");
 else Console.WriteLine("Число четное.");
