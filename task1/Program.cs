6d34f928878d03f7cd037c47f9a3cce978d1e16d
IAPigarev <iapigarev@gmail.com>
2022-11-30 21:29:12 +0300

    added change to the task 1,5

--- task1/Program.cs
+++ task1/Program.cs
@@ -11,18 +11,5 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 int sqweer =number2*number2;
 if (number1==sqweer) Console.WriteLine("Первое число является квадратом второго.");
 else Console.WriteLine("Первое число не является квадратом второго.");
-// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-// 3 -> Среда
-// 5 -> Пятница
-Console.WriteLine("Введите номер дня недели:");
-//string answer = Console.ReadLine();
-int number = Convert.ToInt32(Console.ReadLine());
-if (number==1) Console.WriteLine("Понедельник.");
-if (number==2) Console.WriteLine("Вторник.");
-if (number==3) Console.WriteLine("Среда.");
-if (number==4) Console.WriteLine("Четверг.");
-if (number==5) Console.WriteLine("Пятница.");
-if (number==6) Console.WriteLine("Суббота.");
-if (number==7) Console.WriteLine("Воскресенье.");
-if (number>7)Console.WriteLine("Странный день недели.");
+
 
