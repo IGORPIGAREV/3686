8641fe15f344013b5a398248db1e6ec82b061283
IAPigarev <iapigarev@gmail.com>
2022-11-30 22:29:34 +0300

    added task 8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+int index = 2;
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+while (index<=number)
+{
+Console.Write(index+", ");
+index+=2;
+}
