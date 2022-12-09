a6ccfaed341162d605ce60ac330c0a0b3c05e43c
IAPigarev <iapigarev@gmail.com>
2022-12-09 22:23:54 +0300

    Completed The Task26,28,30

---
+++ task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int ReadInt(string messages)
+{
+    Console.WriteLine(messages);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Razryd(int num)
+{
+    int i=0;
+    for (i=0; num!=0;i++)
+    num=num/10;
+    return i;
+}
+
+int A =ReadInt("Введите число:");
+int result=Razryd(A);
+Console.WriteLine(result);
---
+++ task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+int ReadInt(string messages)
+{
+    Console.WriteLine(messages);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int proiz(int num)
+{
+   
+    int i =1;
+    int pro=1;
+    while (i!=num)
+    {
+    i++;
+    pro=pro*i;
+    }
+    return pro;
+
+}
+
+int A =ReadInt("Введите число:");
+int result=proiz(A);
+Console.WriteLine(result);
