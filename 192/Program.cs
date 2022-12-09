a601e9726c02a1da4e526e6ec59bb45f7917cfc9
IAPigarev <iapigarev@gmail.com>
2022-12-09 17:15:25 +0300

    Completed The Task19

---
+++ 192/Program.cs
@@ -0,0 +1,32 @@
+﻿string InputCheck()// метод ввода и проверки данных.
+{
+int count=0;
+string text=String.Empty;
+while (count != 5)
+{
+    Console.WriteLine("Введите пятизначное число :");
+    text = Console.ReadLine();
+    count = text.Length;
+    if (count!=5) Console.WriteLine("Мимо!"); 
+}
+return text;
+}
+void polydrome(string text)
+{   
+    string answer=String.Empty;
+    int count = text.Length;
+    for (int i = 0; i < count; i++)
+    {
+        count = count-1;
+        if (text[i] != text[count])
+        {
+        answer="нет";
+        break;
+        }
+    else answer="да"; 
+    }
+Console.Write(answer); 
+}
+polydrome(InputCheck());
+
+ 
---
+++ task19.2/Program.cs
@@ -0,0 +1,33 @@
+﻿// for (int count=0; count != 5;)
+// {
+//     Console.WriteLine("Введите пятизначное число :");
+//     string text = Console.ReadLine();
+//     count = text.Length;
+//     if (count!=5) Console.WriteLine("Мимо!"); 
+// }
+int count=0;
+string text=String.Empty;
+while (count != 5)
+{
+    Console.WriteLine("Введите пятизначное число :");
+    text = Console.ReadLine();
+    count = text.Length;
+    if (count!=5) Console.WriteLine("Мимо!");
+}
+// Console.WriteLine("Введите пятизначное число :");
+// string text = Console.ReadLine();
+// int count = text.Length;
+// int cu=Convert.ToInt32(text);
+
+string answer=String.Empty;
+for (int i = 0; i < count; i++)
+{
+    count = count-1;
+    if (text[i] != text[count])
+    {
+        answer="нет";
+        break;
+    }
+    else answer="да"; 
+}
+Console.Write(cu+1); 
